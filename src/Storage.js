bcbdf53971b9d08eac283c86e0a4f858d3f86a97
s749312025 <749312025@qq.com>
2018-03-02 13:43:13 +0800

    react-native-fs

--- src/Storage.js
+++ src/Storage.js
@@ -6,54 +6,6 @@ var storage = new Storage({
 	size: 1000,
 	storageBackend: AsyncStorage,
 	defaultExpires: null,
-	sync: {
-		bookList(params) {
-			let { id, resolve, reject, syncParams: { extraFetchOptions, someFlag } } = params;
-			console.log(extraFetchOptions);
-			fetch("https://api.zhuishushenqi.com/btoc?view=summary&book=" + extraFetchOptions.bookId)
-			.then(list => {
-				return list.json()
-				.then(listJson => {
-					console.log(listJson);
-					fetch(`https://api.zhuishushenqi.com/atoc/${listJson._id}?view=chapters`)
-						.then(menuList => {
-							return menuList.json()
-							.then(menuListJson => {
-								console.log(menuListJson);
-								let data = {
-									data: menuListJson,
-									mark: { index: 0, markCont: "" }
-								}
-								console.log(data);
-								storage.save({
-									key: "bookList",
-									id: extraFetchOptions.bookId,
-									data
-								})
-								return data
-							})
-						})
-				})
-				
-			})
-			// let list = await fetch("https://api.zhuishushenqi.com/btoc?view=summary&book=" + extraFetchOptions.bookId)
-			// let listJson = await list.json()
-			// let sourceId = listJson._id
-			// let menuList = await fetch(`https://api.zhuishushenqi.com/atoc/${sourceId}?view=chapters"`)
-			// let menuListJson = await menuList.json()
-			// let data = {
-			// 	data: menuListJson,
-			// 	mark: { index: 0, markCont: "" }
-			// }
-			// console.log(data);
-			// storage.save({
-			// 	key: "bookList",
-			// 	id: extraFetchOptions.bookId,
-			// 	data
-			// })
-			// return data
-		}
-	}
 })
 
 global.Storage = storage
--- src/pages/text.js
+++ src/pages/text.js
@@ -1,41 +1,63 @@
 import React, { Component } from 'react';
-import { View, Text, WebView } from 'react-native';
+import { View, WebView } from 'react-native';
 
 class Test extends Component {
 	constructor(props) {
 		super(props);
-		
+		this.state = {
+			sourceId: ""
+		}
 	}
 	_onMessage = async (e) => {
-		//let data = await e.nativeEvent.data.json()
-		console.log(JSON.parse(e.nativeEvent.data))
+		//console.log(e.nativeEvent.data);
+		this.props.save(JSON.parse(e.nativeEvent.data))
 	}
+	
+	componentWillMount() {
+		this.getSourceId()
+		
+	}
+	getSourceId = async () => {
+		let isCanget = true
+		//http://api.zhuishushenqi.com/btoc/5624fc7549d2a9a66871df4e?view=chapters&channel=mweb
+		let list = await fetch("https://api.zhuishushenqi.com/btoc?view=summary&book=" + this.props.bookid)
+		let listJson = await list.json()
+		if (listJson.length == 0) {
+			isCanget = false;
+			let list = await fetch("https://api.zhuishushenqi.com/ctoc?view=summary&book=" + this.props.bookid)
+			let listJson = await list.json()
+			this.setState({ sourceId: listJson[0]._id })
+		}else {
+			this.setState({ sourceId: listJson[0]._id })
+		}
+	}
+	
 	render() {
-		const xx = `document.addEventListener('message', function (e) {
+		const data = `document.addEventListener('message', function (e) {
 									var request = new XMLHttpRequest();
 									request.onreadystatechange = (e) => {
-							       if (request.readyState !== 4) {
-								         return;
-							       }
-							       if (request.status === 200) {
-												window.postMessage(request.responseText);
-							       } else {
-													window.postMessage(request.responseText);
-							       }
-							     };
-									request.open('GET', 'https://api.zhuishushenqi.com/atoc/574449db9eeb98130f1320a2?view=chapters');
-		     					request.send();
+										if (request.readyState !== 4) {
+											return;
+										}
+										if (request.status === 200) {
+											window.postMessage(request.responseText);
+										} else {
+											window.postMessage(request.responseText);
+										}
+									};
+									request.open('GET', 'https://api.zhuishushenqi.com/atoc/${this.state.sourceId}?view=chapters');
+									request.send();
 								});`
 		return (
-			<View style={{flex: 1}}>
-				<WebView
-					style={{flex:1}}
-					ref={'webview'}
-					source={{ uri: "https://api.zhuishushenqi.com/ranking/gender"}}
-					injectedJavaScript={xx}
-					onLoadEnd={() => { this.refs.webview.postMessage(22);}}
-					onMessage= {this._onMessage}
-				/>
+			<View>
+				{this.state.sourceId == "" ? null : 
+					<WebView
+						ref={'webview'}
+						source={{ uri: "https://api.zhuishushenqi.com/ranking/gender" }}
+						injectedJavaScript={data}
+						onLoadEnd={() => { this.refs.webview.postMessage(); }}
+						onMessage={this._onMessage}
+					/>}
 			</View>
 		);
 	}
